d2bd5ef872785749e179b1f8437a48fdadf4fbc5
BENDeR-BK <bogdanKvasov@gmail.com>
2021-03-19 18:12:12 +0200

    qqq

--- js/common.js
+++ js/common.js
@@ -121,5 +121,202 @@ $(document).ready(function () {
     });
 
 
+
     
+    // product
+
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav',
+        responsive: [
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: true
+                }
+            }
+        ]
+    });
+    $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: false,
+        centerMode: false,
+        focusOnSelect: true,
+        vertical: true,
+        arrows: false,
+        verticalSwiping: true
+    });
+
+    // prod icons
+    $('.prod-icons .favorite').click(function () {
+        $(this).toggleClass('active');
+    });
+
+    $('.descr-accordeon__sect .title').click(function () {
+        $('.descr-accordeon__sect .descr').not($(this).parents('.descr-accordeon__sect').find('.descr')).slideUp();
+        $(this).parents('.descr-accordeon__sect').find('.descr').slideToggle();
+
+        $('.descr-accordeon__sect').not($(this).parents('.descr-accordeon__sect')).removeClass('open');
+        $(this).parents('.descr-accordeon__sect').toggleClass('open');
+    });
+
+    // Product slider
+
+
+    var $prodslCount = $('.alsolike-sliderwrap .pagingInfo');
+    var $likeslider = $('.alsolike__slider');
+
+    $likeslider.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+        if (slick.slideCount < 10) {
+            var i = (currentSlide ? currentSlide : 0) + 1;
+            $prodslCount.text('/ 0' + i);
+        } else {
+            var i = (currentSlide ? currentSlide : 0) + 1;
+            $prodslCount.text('/' + i);
+        }
+
+    });
+
+    $likeslider.slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        dots: false,
+        infinite: true,
+        prevArrow: '<button type="button" class="slick-prev">Prev</button>',
+        nextArrow: '<button type="button" class="slick-next">Next</button>',
+        autoplay: true,
+        autoplaySpeed: 5000,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        speed: 600,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 421,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
+    });
+
+    // search
+    $('.header__blackline-search span').click(function () {
+        $('.search-wrapper').show();
+    });
+
+    $('.search-wrapper .close').click(function () {
+        $('.search-wrapper').hide();
+    });
+
+    
+    // size
+
+    $('.size-tabbtns .in-btn').click(function () {
+        $('.size-tabbtns span').removeClass('active');
+        $(this).addClass('active');
+        $('.sizepbody-tabcont').hide();
+        $('.sizepbody-tabcont.sizepbody-in').show();
+    });
+
+    $('.size-tabbtns .cm-btn').click(function () {
+        $('.size-tabbtns span').removeClass('active');
+        $(this).addClass('active');
+        $('.sizepbody-tabcont').hide();
+        $('.sizepbody-tabcont.sizepbody-cm').show();
+    })
+
+
+    $('.sizeopen').click(function (event) {
+        event.preventDefault();
+        $('.size-popup__wrap').show();
+    });
+    $('.close-size').click(function () {
+        $('.size-popup__wrap').hide();
+    });
+    $(document).click(function (event) {
+        let $target = $(event.target);
+        if (!$target.closest('.size-popup').length && !$target.closest('.sizeopen').length) {
+            $('.size-popup__wrap').hide();
+        }
+    });
+
+    // login
+    $(".showpass").click(function () {
+        var input = $('.pass input');
+        if (input.attr("type") == "password") {
+            input.attr("type", "text");
+        } else {
+            input.attr("type", "password");
+        }
+    });
+
+    $('.contact-link').click(function () {
+        $('.mc-log').hide();
+        $('.mc-cont').show();
+    });
+
+
+
+    // log popups
+    $('.lostpass-link').click(function () {
+        $('.mc-gen').hide();
+        $('.mc-forgotpass').show();
+    });
+
+
+    $('.contact-link').click(function () {
+        $('.mc-gen').hide();
+        $('.mc-cont').show();
+    });
+
+    $('.reglink').click(function () {
+        $('.mc-gen').hide();
+        $('.mc-reg').show();
+    });
+
+    $('.log-link').click(function () {
+        $('.mc-gen').hide();
+        $('.mc-log').show();
+    });
+
+
+
+
+
+
+
+    $(document).click(function (event) {
+        let $target = $(event.target);
+        if (!$target.closest('.login-modal-dialog.gdial').length) {
+            $('.mc-gen').hide();
+            $('.mc-log').show();
+        }
+    });
+    $('.loginpopup .close').click(function () {
+        $('.mc-gen').hide();
+        $('.mc-log').show();
+    });
+
 });
